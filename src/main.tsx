702fb12ceecbaf25e12b933e7c27c905a901b9d1
WA ANGIELSKI <ahmed.backerjr@gmail.com>
2026-04-19 20:28:27 +0200

    fix: relocate core logic to escape vercel build cache

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RootContainer from './RootContainer'
+import AppMain from './core/AppMain'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RootContainer />
+    <AppMain />
   </React.StrictMode>,
 )
